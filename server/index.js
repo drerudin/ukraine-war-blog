ef7d7746f4550ce6aa41f224e0c24f53ec268d70
Andrey Rudinskiy <drerudin@gmail.com>
2022-09-09 21:44:41 -0700

    created blog routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import blogRouter from './routes/blog-routes';
 import router from "./routes/user-routes";
 const app = express();
 
 app.use(express.json());
 app.use("/api/user", router) 
+app.use("/api/blog", blogRouter);
 mongoose.connect('mongodb+srv://admin:andrey1234@cluster0.jczpais.mongodb.net/ukrblog?retryWrites=true&w=majority')
 .then(()=>app.listen(4000))
 .then(()=>console.log("Connected to the database and listening to localhost 4000"))
