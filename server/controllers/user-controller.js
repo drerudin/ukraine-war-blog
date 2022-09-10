cc378790e947f3e272a4cd57b04b633612f41035
Andrey Rudinskiy <drerudin@gmail.com>
2022-09-09 21:32:25 -0700

    user login access created

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,4 +1,5 @@
 import User from "../model/User";
+import bcrypt from "bcryptjs";
 
 export const getAllUsers = async (req, res, next) => {
     let users;
@@ -24,10 +25,11 @@ export const signup = async (req, res, next) => {
     if(existingUser) {
         return res.status(400).json({message: "User already exists, please login instead."});
     }
+    const hashedPassword = bcrypt.hashSync(password);
     const user = new User({
         name,
         email,
-        password
+        password: hashedPassword
     });
 
     try {
@@ -36,4 +38,21 @@ export const signup = async (req, res, next) => {
      return console.log(err)
     }
     return res.status(201).json({user})
+}
+
+export const login = async(req,res,next) => {
+    const { email, password } = req.body;
+    try {
+        existingUser = await User.findOne({email});
+    }catch(err) {
+    return console.log(err);
+    }
+    if(existingUser) {
+        return res.status(404).json({message: "Couldnt find user by this email."});
+    }
+    const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+    if(!isPasswordCorrect) {
+        return res.status(400).json({message: "Wrong Password"})
+    }
+    return res.status(200).json({message: "Login Successful"})
 }
