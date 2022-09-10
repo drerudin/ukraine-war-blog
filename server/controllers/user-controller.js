c9c78698c96e6eb30879a9b03b00309daeda8281
Andrey Rudinskiy <drerudin@gmail.com>
2022-09-09 21:19:59 -0700

    user signup created

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -12,3 +12,28 @@ export const getAllUsers = async (req, res, next) => {
     }
     return res.status(200).json({users});
 }
+
+export const signup = async (req, res, next) => {
+    const {name, email, password} = req.body;    
+    let existingUser;
+    try {
+        existingUser = await User.findOne({email});
+    }catch(err) {
+    return console.log(err);
+    }
+    if(existingUser) {
+        return res.status(400).json({message: "User already exists, please login instead."});
+    }
+    const user = new User({
+        name,
+        email,
+        password
+    });
+
+    try {
+        await user.save();
+    }catch (err) {
+     return console.log(err)
+    }
+    return res.status(201).json({user})
+}
--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 import router from "./routes/user-routes";
 const app = express();
 
-
+app.use(express.json());
 app.use("/api/user", router) 
 mongoose.connect('mongodb+srv://admin:andrey1234@cluster0.jczpais.mongodb.net/ukrblog?retryWrites=true&w=majority')
 .then(()=>app.listen(4000))
