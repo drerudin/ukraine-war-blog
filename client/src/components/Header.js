64aa3b74bcf85a5c9db086e9f3a8bcc93e37cc2e
Andrey Rudinskiy <drerudin@gmail.com>
2022-09-10 21:47:07 -0700

    added header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import {AppBar, Typography, Toolbar, Button, Box} from '@mui/material'
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+  <AppBar
+  sx={{
+    background: "linear-gradient(0deg, rgba(235,251,97,1) 0%, rgba(4,152,255,1) 100%)"
+  }}>
+    <Toolbar>
+      <Typography variant="h4">Ukraine War Blog</Typography>
+      <Box display="flex" marginLeft="auto">
+        <Button variant='contained' sx={{margin:1, borderRadius:10}} color="inherit">Login</Button>
+        <Button variant='contained' sx={{margin:1, borderRadius:10}} color="inherit">Signup</Button>
+      </Box>
+    </Toolbar>
+  </AppBar>
+  );
+};
 
 export default Header
