f5de28b590c290fa0867123e638fc843f1a346b4
Andrey Rudinskiy <drerudin@gmail.com>
2022-09-10 22:33:20 -0700

    header completed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import {AppBar, Typography, Toolbar, Button, Box} from '@mui/material'
+import {AppBar, Typography, Toolbar, Button, Box, Tabs, Tab} from '@mui/material'
+import {Link} from 'react-router-dom'
+import {useState} from 'react'
 
 const Header = () => {
+  const [value, setValue] = useState();
   return (
   <AppBar
+    position="sticky"
   sx={{
     background: "linear-gradient(0deg, rgba(235,251,97,1) 0%, rgba(4,152,255,1) 100%)"
   }}>
     <Toolbar>
       <Typography variant="h4">Ukraine War Blog</Typography>
+      { isLoggedIn && <Box display="flex" marginLeft={'auto'} marginRight={'auto'}>
+        <Tabs textColor="inherit" value={value} onChange={(e,val)=>setValue(val)}>
+          <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
+          <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs" />
+        </Tabs>
+      </Box> }
       <Box display="flex" marginLeft="auto">
-        <Button variant='contained' sx={{margin:1, borderRadius:10}} color="inherit">Login</Button>
-        <Button variant='contained' sx={{margin:1, borderRadius:10}} color="inherit">Signup</Button>
+       { !isLoggedIn && <> <Button LinkComponent={Link} to="/auth" variant='contained' sx={{margin:1, borderRadius:10}} color="warning">Login</Button>
+        <Button LinkComponent={Link} to="/auth" variant='contained' sx={{margin:1, borderRadius:10}} color="warning">Signup</Button> </> }
+       { isLoggedIn && <Button LinkComponent={Link} to="/auth" variant='contained' sx={{margin:1, borderRadius:10}} color="warning">Logout</Button>}
       </Box>
     </Toolbar>
   </AppBar>
